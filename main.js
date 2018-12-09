2fd4062563f042f61814f0de69a57e75b3220ebb
ray-nis <reirei256@gmail.com>
2018-12-09 20:58:38 +0100

    onscroll

--- main.js
+++ main.js
@@ -10,6 +10,28 @@ menuIcon.addEventListener("click", ()=> {
     }});
 
 
+///
+
 let navBtn = document.getElementById("nav-btn");
 
 navBtn.addEventListener("click", ()=> {document.getElementById('services').scrollIntoView();})
+
+
+///
+
+let navHeader = document.getElementById("top-bar");
+
+function runOnScroll() {
+    let windowY = Math.ceil(window.scrollY);
+    console.log(windowY);
+    if (windowY < 350) {
+      navHeader.style.height = "10vh";
+      document.getElementById("services-link").classList.remove("active-state");
+      }
+    else if (windowY > 350) {
+        navHeader.style.height = "7vh";
+        document.getElementById("services-link").classList.add("active-state");
+    }
+ }; 
+
+ window.addEventListener("scroll", runOnScroll);
